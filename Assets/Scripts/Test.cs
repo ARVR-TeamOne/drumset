85c1cf90d9e9d54b5e8f51785698eee830546e23
Jonas Leitner <jonas.leitner2@stud.fh-flensburg.de>
2017-12-08 19:17:43 +0100

    not tracking and line rendering working

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject[] arrayPrefab;
+
+    // Use this for initialization
+    void Start () {
+        arrayPrefab = new GameObject[10];
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 position = new Vector3(3*i, 5+3*i, i*1);
+            Quaternion rotation = new Quaternion(1, 2, 1, 1);
+            GameObject obj = Instantiate(prefab, position, rotation) as GameObject;
+            arrayPrefab[i] = obj;
+        }
+        this.changedMethode(arrayPrefab);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void changedMethode(GameObject[] array)
+    {   //
+        array[0].transform.position = new Vector3(0,0,0);
+        var startPos = array[0].transform.position;
+
+        foreach (GameObject test in array)
+        {
+            test.transform.rotation = Quaternion.FromToRotation(Vector3.left, new Vector3(0, 0, 0));
+            print(test.transform.rotation);
+            print(startPos - test.transform.position);
+            test.transform.Translate(0, startPos.y - test.transform.position.y,0);
+        }
+    }
+}
--- Assets/Scripts/TrackScript.cs
+++ Assets/Scripts/TrackScript.cs
@@ -4,56 +4,115 @@ using UnityEngine;
 using Vuforia;
 
 public class TrackScript : MonoBehaviour {
-    
+
+    public GameObject TrackSlider;
+
     private GameObject ImageTargetTrackStart;
     private GameObject ImageTargetTrackEnd;
-    private LineRenderer TrackLine;
-
+    private TrackTargetTrackableEventHandler ImageTargetTrackStartTrackable;
+    private TrackTargetTrackableEventHandler ImageTargetTrackEndTrackable;
+    private LineRenderer MainTrackLine;
+    private List<GameObject> TrackLines = new List<GameObject>();
+    private List<Note> NoteList = new List<Note>();
+    private const float TRACK_DURATION = 8f;            //playtime of the track in seconds
 
     // Use this for initialization
     void Start () {
-        ImageTargetTrackStart = GameObject.Find("ImageTargetTrackStart");
-        ImageTargetTrackEnd = GameObject.Find("ImageTargetTrackEnd");
+        ImageTargetTrackStart = GameObject.FindGameObjectWithTag("ImageTargetTrackStart");
+        ImageTargetTrackStartTrackable = ImageTargetTrackStart.GetComponent<TrackTargetTrackableEventHandler>();
+        ImageTargetTrackEnd = GameObject.FindGameObjectWithTag("ImageTargetTrackEnd");
+        ImageTargetTrackEndTrackable = ImageTargetTrackEnd.GetComponent<TrackTargetTrackableEventHandler>();
 
         //create track line object
-        TrackLine = this.gameObject.AddComponent<LineRenderer>();
+        MainTrackLine = this.gameObject.AddComponent<LineRenderer>();
         // Set the width of the Line Renderer
-        TrackLine.SetWidth(0.005F, 0.005F);
+        MainTrackLine.SetWidth(0.005F, 0.015F);
+        MainTrackLine.SetColors(new Color(255, 255, 255), new Color(0, 255, 0));
         // Set the number of vertex fo the Line Renderer
-        TrackLine.SetVertexCount(2);
+        MainTrackLine.SetVertexCount(2);
+        //draw trackline
+        MainTrackLine.SetVertexCount(2);
     }
 	
 	// Update is called once per frame
-	void Update () {
-
-        if(ImageTargetTrackStart.GetComponent<TrackableBehaviour>().CurrentStatus == TrackableBehaviour.Status.TRACKED && ImageTargetTrackEnd.GetComponent<TrackableBehaviour>().CurrentStatus == TrackableBehaviour.Status.TRACKED)
+	void Update ()
+    {
+        //clear all linerenderers
+        foreach(GameObject lineObject in TrackLines)
         {
-            //draw trackline
-            TrackLine.SetVertexCount(2);
-            TrackLine.SetPosition(0, ImageTargetTrackStart.transform.position);
-            TrackLine.SetPosition(1, ImageTargetTrackEnd.transform.position);
+            Destroy(lineObject);
+        }
+
+        TrackLines.Clear();
+
+        if (ImageTargetTrackStart.GetComponent<TrackTargetTrackableEventHandler>().status == TrackableBehaviour.Status.TRACKED && ImageTargetTrackEnd.GetComponent<TrackTargetTrackableEventHandler>().status == TrackableBehaviour.Status.TRACKED)
+        {
+
+            MainTrackLine.SetPosition(0, ImageTargetTrackStart.transform.position);
+            MainTrackLine.SetPosition(1, ImageTargetTrackEnd.transform.position);
 
             /*
              * calculate note positions
              */
+
+            LineRenderer NoteLine;
+
             //for each vumark
-            foreach(GameObject Note in GameObject.FindGameObjectsWithTag("Note"))
+            foreach (GameObject Note in GameObject.FindGameObjectsWithTag("Note"))
             {
                 if (Note.GetComponent<TrackableBehaviour>().CurrentStatus == TrackableBehaviour.Status.TRACKED)
                 {
-                    //calculate orthogonal angle to trackline
+                    //translate note onto surface between trackstart and trackend
+                    Vector3 trackSurfaceVector = ImageTargetTrackStart.transform.position - ImageTargetTrackEnd.transform.position;
+                    Note.transform.rotation = Quaternion.FromToRotation(ImageTargetTrackStart.transform.position, ImageTargetTrackEnd.transform.position);
+                    Note.transform.Translate(0, trackSurfaceVector.y, 0);
 
-                    //get rotation on 2d surface
+                    //get orthogonal angle from trackVector to note
+                    Vector3 noteTrackPosition = NearestPointOnLine(ImageTargetTrackStart.transform.position, ImageTargetTrackStart.transform.position - ImageTargetTrackEnd.transform.position, Note.transform.position);
+                    Debug.Log(noteTrackPosition);
+
+                    //draw noteline
+                    TrackLines.Add(new GameObject());
+                    NoteLine = TrackLines[TrackLines.Count - 1].gameObject.AddComponent<LineRenderer>();
+                    // Set the width of the Line Renderer
+                    NoteLine.SetWidth(0.005F, 0.015F);
+                    NoteLine.SetColors(new Color(0, 255, 255), new Color(0, 255, 0));
+                    // Set the number of vertex fo the Line Renderer
+                    NoteLine.SetVertexCount(2);
+
+                    NoteLine.SetPosition(0, noteTrackPosition);
+                    NoteLine.SetPosition(1, Note.transform.position);
                 }
             }
 
             /*
              * calculate sounds
              */
-        } else
-        {
-            //hide trackline
-            TrackLine.SetVertexCount(0);
         }
+    }
+
+    //linePnt - point the line passes through
+    //lineDir - unit vector in direction of line, either direction works
+    //pnt - the point to find nearest on line for
+    public static Vector3 NearestPointOnLine(Vector3 linePnt, Vector3 lineDir, Vector3 pnt)
+    {
+        lineDir.Normalize();//this needs to be a unit vector
+        var v = pnt - linePnt;
+        var d = Vector3.Dot(v, lineDir);
+        return linePnt + lineDir * d;
+    }
+}
+
+class Note
+{
+    public float TrackTimestamp;
+    public float Pitch;
+    public float Volume;
+
+    public Note(float timestamp, float pitch, float volume)
+    {
+        TrackTimestamp = timestamp;
+        Pitch = pitch;
+        Volume = volume;
     }
 }
